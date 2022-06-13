e3cbcefd3a1a62fccf790196c812e8fb3b31eee7
saransh-283 <saranshmm@gmail.com>
2022-06-13 20:13:16 +0530

    Basic page setup

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const express = require("express");
-const app = express();
+const express = require("express"); // Imports express
+const app = express(); // Initializes express app
 
-const PORT = 8080;
+const PORT = 8080; // Port on which to run the server
 
+// Handle get requests to /
 app.get("/", (req, res) => {
-  res.send({ msg: "Hello" });
+  res.sendFile("./view/landing.html");
 });
 
+// Handle get requests to /home
+app.get("/", (req, res) => {
+  res.sendFile("./view/home.html");
+});
+
+// Handle get requests to /student
+app.get("/", (req, res) => {
+  res.sendFile("./view/student.html");
+});
+
+// Handle get requests to /teacher
+app.get("/", (req, res) => {
+  res.sendFile("./view/teacher.html");
+});
+
+// Handle get requests to /admin
+app.get("/", (req, res) => {
+  res.sendFile("./view/admin.html");
+});
+
+// Server starts listening to the port
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
